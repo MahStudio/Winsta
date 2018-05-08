26dd16651765ef0d718154abf6c07d0028d38c6a
ngame <ngame@DESKTOP-ESFCCS8>
2018-05-08 17:27:11 +0430

    -Added followers view

--- WinGoTag/DataBinding/GeneratorIncrementalLoadingClass.cs
+++ WinGoTag/DataBinding/GeneratorIncrementalLoadingClass.cs
@@ -150,4 +150,54 @@ namespace WinGoTag.DataBinding
 
         #endregion 
     }
+
+    public class GenerateUserFollowers<T> : IncrementalLoadingBase
+    {
+        private string _username;
+        private int _LastPage = 1;
+        PaginationParameters pagination;
+        public GenerateUserFollowers(uint maxCount, Func<int, T> generator, string username)
+        {
+            _generator = generator;
+            _maxCount = maxCount;
+            _username = username;
+            pagination = PaginationParameters.MaxPagesToLoad(1);
+        }
+
+        protected async override Task<IList<object>> LoadMoreItemsOverrideAsync(System.Threading.CancellationToken c, uint count)
+        {
+            uint toGenerate = System.Math.Min(count, _maxCount - _count);
+            // Wait for work 
+            await Task.Delay(10);
+            //http://getsongg.com/dapp/getnewcases?lang=en&tested
+            IEnumerable<InstaUserShort> tres = null;//
+            var res = await AppCore.InstaApi.GetUserFollowersAsync(_username, pagination);
+            pagination.NextId = res.Value.NextId;
+            tres = res.Value.ToList();
+            // This code simply generates
+            if (tres == null)
+            {
+                return (new List<InstaMedia>()).ToArray();
+            }
+            var values = from j in Enumerable.Range((int)_count, (int)toGenerate)
+                         select (object)_generator(j);
+            _count += Convert.ToUInt32(tres.Count());
+            _LastPage++;
+            //App._MainPageInt++;
+            return tres.ToArray();
+        }
+
+        protected override bool HasMoreItemsOverride()
+        {
+            return _count < _maxCount;
+        }
+
+        #region State
+
+        Func<int, T> _generator;
+        uint _count = 0;
+        uint _maxCount;
+
+        #endregion 
+    }
 }
