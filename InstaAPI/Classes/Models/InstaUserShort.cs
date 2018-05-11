4e258b0772e06ecf6f150a8b0f85cd1bc8cab6b6
ngame <ngame@DESKTOP-ESFCCS8>
2018-05-11 19:49:24 +0430

    -Follow accept UI

--- InstaAPI/Classes/Models/InstaUserShort.cs
+++ InstaAPI/Classes/Models/InstaUserShort.cs
@@ -9,6 +9,7 @@ namespace InstaSharper.Classes.Models
         public bool IsPrivate { get; set; }
         public long Pk { get; set; }
         public string ProfilePicture { get; set; }
+        public string profile_pic_url { get; set; }
         public string ProfilePictureId { get; set; } = "unknown";
         public string UserName { get; set; }
         public string FullName { get; set; }
---
+++ WinGoTag/View/ActivityView/FollowRequestsView.xaml.cs
@@ -0,0 +1,60 @@
+﻿using InstaSharper.Classes.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace WinGoTag.View.ActivityView
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class FollowRequestsView : Page
+    {
+        public FollowRequestsView()
+        {
+            this.InitializeComponent();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+            AppCore.ModerateBack(Frame.GoBack);
+            myList.ItemsSource = e.Parameter;
+        }
+
+        private void ToBackBT_Click(object sender, RoutedEventArgs e)
+        {
+            Frame.GoBack();
+        }
+
+        private void User_ItemClick(object sender, ItemClickEventArgs e)
+        {
+
+        }
+
+        private void Accept_Click(object sender, RoutedEventArgs e)
+        {
+            var user = (sender as Button).Tag as InstaUserShort;
+            AppCore.InstaApi.AcceptFriendshipRequest(user.Pk);
+        }
+
+        private void Ignore_Click(object sender, RoutedEventArgs e)
+        {
+            var user = (sender as Button).Tag as InstaUserShort;
+            AppCore.InstaApi.IgnoreFriendshipRequest(user.Pk);
+        }
+    }
+}
