34e77ae95b02eeefb4522c0f9f7dc3084ce488bb
Aston771 <aston771@live.it>
2018-05-16 17:51:40 +0200

    -fix

--- WinGoTag/View/AddPhotos/EditItem/AddEditItem.cs
+++ WinGoTag/View/AddPhotos/EditItem/AddEditItem.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Lumia.Imaging.Adjustments;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
@@ -9,20 +10,20 @@ namespace WinGoTag.View.AddPhotos.EditItem
 {
     public class AddEditItem
     {
-        public ObservableCollection<GridViewEditItem> HamburgerMenuItems =
+        public ObservableCollection<GridViewEditItem> GridViewItems =
              new ObservableCollection<GridViewEditItem>();
         //                          Adjust    Bright    Contrast           saturion    Color     Fade
         private string[] icons = { "\ue78A", "\ue706", "\ue793", "\ue7BA", "\ueB42", "\ueF3C", "\ue753" };
 
         private string[] texts = { "Adjust", "Brightness", "Contrast", "Warmth", "Saturation", "Color", "Fade", "Highlighths", "Shadows", "Vignette", "Tilt Shift", "Sharpen" };
 
-        private Type[] frames = { null, null, null, null };
+        private Type[] Effect = { null, null, null, null };
 
         public AddEditItem()
         {
             for (int i = 0; i < icons.Length; i++)
             {
-                HamburgerMenuItems.Add(new GridViewEditItem()
+                GridViewItems.Add(new GridViewEditItem()
                     {
 
                         Icon = icons[i],
