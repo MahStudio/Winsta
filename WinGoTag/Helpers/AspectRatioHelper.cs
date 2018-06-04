4b307988f8124302a694d0a4c93decc7206131de
ngame <ngame@DESKTOP-ESFCCS8>
2018-06-04 11:37:35 +0430

    -aspectRatio helper

---
+++ WinGoTag/Helpers/AspectRatioHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinGoTag.Helpers
+{
+    class AspectRatioHelper
+    {
+        public static float GetAspectRatioForMedia(int Height, int Width)
+        {
+            float ratio = (float)Width / Height;
+            return ratio;
+        }
+    }
+}
