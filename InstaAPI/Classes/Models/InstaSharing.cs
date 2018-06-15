4bbad436de9c53b802addf06bf60c0084ad44763
Ramtinak <ramtinak@live.com>
2018-06-15 16:45:55 +0430

    Photo story sharing added

--- InstaAPI/Classes/Models/InstaSharing.cs
+++ InstaAPI/Classes/Models/InstaSharing.cs
@@ -6,19 +6,24 @@ using System.Threading.Tasks;
 using Newtonsoft.Json;
 namespace InstaSharper.Classes.Models
 {
-    public class InstaStorySharing
+    public enum SharingType
+    {
+        Video,
+        Photo
+    }
+    public class InstaSharing
     {
         [JsonProperty("action")]
         public string Action { get; set; }
         [JsonProperty("status_code")]
         public string StatusCode { get; set; }
         [JsonProperty("payload")]
-        public InstaStorySharingPayload[] Payload { get; set; }
+        public InstaSharingPayload[] Payload { get; set; }
         [JsonProperty("status")]
         public string Status { get; set; }
     }
 
-    public class InstaStorySharingPayload
+    public class InstaSharingPayload
     {
         [JsonProperty("thread_id")]
         public string ThreadId { get; set; }
