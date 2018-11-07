e23a3ff289d0f4252d5106ad647e9eb8a5d35cd3
NGame <ngame1390@hotmail.com>
2018-11-07 21:46:30 +0330

    End of the open source

--- WinGoTag/View/AddPhotos/EditItem/EditItem.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace WinGoTag.View.AddPhotos.EditItem
-{
-    public class GridViewEditItem
-    {
-        public string Icon { get; set; }
-        public string Text { get; set; }
-        public int Target { get; set; }
-    }
-}
